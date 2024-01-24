afe04df7575ac1aaf7d6f768d89747ae92667696
dze <58722837+marilynnn@users.noreply.github.com>
2024-01-24 16:27:19 +0300

    client + serv

--- lab4.py
+++ lab4.py
@@ -1,22 +1,181 @@
-from aiohttp import web
 import json
 import datetime
+from aiohttp import web
 
 
+# класс лабораторной работы, для инициализации обязательно нужно указать дедлайн
 class Lab:
-    '''Класс лабораторной работы'''
-    def __init__(self, name, date):
-        self.name = name  # название лабораторной
-        self.date = datetime.datetime.strptime(date, '%d.%m.%Y')  # дата сдачи
-        self.descr = None  # описание лабораторной (опционально)
-        self.students = []  # список студентов, сдавших данную лабораторную работу (опционально)
+    def __init__(self, str_date):
+        self.description = ""  # описание лабораторной
+        self.date = datetime.datetime.strptime(str_date, '%d.%m.%Y')  # дедлайн
+        self.students = list()  # список студентов
 
-    def set_descr(self, descr):
-        self.descr = descr
+    def set_description(self, description):  # обновить описание (очищает старое описание)
+        self.description = description
 
-    def add_students(self, students):
-        if hasattr(students, '__iter__'):
-            self.students.extend(students)
-        else:
-            self.students.append(students)
+    def set_deadline(self, str_date):  # обновить дедлайн
+        self.date = datetime.datetime.strptime(str_date, '%d.%m.%Y')
 
+    @staticmethod
+    def process_query_str(string):
+        string.replace(" ", "")
+        string.replace("[", "")
+        string.replace("]", "")
+        string.replace("\'", "")
+
+    def add_students(self, string):   # добавить студентов в конец списка
+        string_without_spaces = string.replace(" ", "")
+        students_list = string_without_spaces.strip().split(',')
+        self.students = self.students + students_list
+        print(self.students)
+
+    def delete_students(self, string):  # удалить студентов
+        string_without_spaces = string.replace(" ", "")
+        for student in string_without_spaces.strip().split(','):
+            if student in self.students:
+                self.students.remove(student)
+
+    def update_students(self, string):  # очистить список студентов и задать новый
+        string_without_spaces = string.replace(" ", "")
+        self.students = string_without_spaces.strip().split(',')
+
+    def __str__(self):  # вся информация о лабораторной
+        s = '{{\"deadline\" : \"{0}\", \"description\" : \"{1}\", \"students\" : \"{2}\"}}'\
+            .format(self.date.strftime('%d.%m.%Y'), self.description, ", ".join(self.students))
+        return s
+
+
+# класс сервера
+class Server:
+    def __init__(self):
+        self.app = web.Application()
+        self.host = 'localhost'
+        self.port = 8080
+        self.labs = dict()
+        self.url = 'http://' + self.host + ':' + str(self.port) + '/labs'
+
+        #  добавляем все нужные маршруты и связываем запросы с функциями
+        self.app.router.add_post('/labs', self.handle_create_lab)
+        self.app.router.add_get('/labs', self.handle_get_all_labs)
+        self.app.router.add_patch('/labs/{name}', self.handle_edit_lab)
+        self.app.router.add_get('/labs/{name}', self.handle_get_lab)
+        self.app.router.add_delete('/labs/{name}', self.handle_delete_lab)
+
+    #  функция для GET-запроса информации о всех лабораторных
+    async def handle_get_all_labs(self, request):
+        try:
+            response_obj = {"status": "success"}
+            if len(self.labs) == 0:
+                response_obj["message"] = "no labs"
+            else:
+                for key in self.labs:
+                    response_obj[key] = str(self.labs[key])
+
+            return web.Response(text=json.dumps(response_obj), status=200)
+
+        except Exception as ex:
+            print("Getting all labs failed")
+
+            response_obj = {"status": "failed", "message": str(ex)}
+            return web.Response(text=json.dumps(response_obj), status=400)
+
+    #  функция для POST-запроса на создание лабораторной работы, обязательно нужно указать дедлайн,
+    #  опционально -- описание
+    async def handle_create_lab(self, request):
+        try:
+            # print(request.query)
+            lab_name = request.query["name"]
+            lab_deadline = request.query["date"]
+            self.labs[lab_name] = Lab(lab_deadline)
+            if "description" in request.query:
+                self.labs[lab_name].set_description(request.query["description"])
+
+            response_obj = {"status": "success", 'Location': "{0}/{1}".format(self.url, lab_name)}
+            return web.Response(text=json.dumps(response_obj), status=201)
+
+        except Exception as ex:
+            print("Creating new lab failed", str(ex))
+
+            response_obj = {"status": "failed", "message": str(ex)}
+            return web.Response(text=json.dumps(response_obj), status=400)
+
+    #  функция для PATCH-запроса на изменение существующей лабораторной
+    async def handle_edit_lab(self, request):
+        try:
+            lab_name = self.get_lab_name(request)
+
+            if lab_name not in self.labs:
+                response_obj = {"status": "failed", "message": str('lab with name \'{}\' doesn\'t exist'.format(lab_name))}
+                print(self.labs)
+                return web.Response(text=json.dumps(response_obj), status=404)
+
+            current_lab = self.labs[lab_name]
+            if "description" in request.query:
+                current_lab.set_description(request.query["description"])
+            if "date" in request.query:
+                current_lab.set_deadline(request.query["date"])
+            if "students" in request.query:
+                current_lab.update_students(request.query["students"])
+            if "add_students" in request.query:
+                # print(type(request.query["add_students"]))
+                current_lab.add_students(request.query["add_students"])
+            if "delete_students" in request.query:
+                current_lab.delete_students(request.query["delete_students"])
+
+            response_obj = {"status": "success", "message": 'lab is updated'}
+            return web.Response(text=json.dumps(response_obj), status=200)
+
+        except Exception as ex:
+            print("Editing lab failed", ex)
+
+            response_obj = {"status": "failed", "message": str(ex)}
+            return web.Response(text=json.dumps(response_obj), status=400)
+
+    #  функция GET-запроса информации по конкретной лабораторной
+    async def handle_get_lab(self, request):
+        try:
+            lab_name = self.get_lab_name(request)
+
+            if lab_name not in self.labs:
+                response_obj = {"status": "failed", "message": str('lab with name \'{}\' doesn\'t exist'.format(lab_name))}
+                return web.Response(text=json.dumps(response_obj), status=404)
+
+            current_lab = self.labs[lab_name]
+
+            response_obj = {"status": "success", lab_name: str(current_lab)}
+            return web.Response(text=json.dumps(response_obj), status=200)
+
+        except Exception as ex:
+            print("Getting lab failed")
+
+            response_obj = {"status": "failed", "message": str(ex)}
+            return web.Response(text=json.dumps(response_obj), status=400)
+
+    #  функция DELETE-запроса на удаление лабораторной
+    async def handle_delete_lab(self, request):
+        try:
+            lab_name = self.get_lab_name(request)
+
+            if lab_name not in self.labs:
+                response_obj = {"status": "failed", "message": str('lab with name \'{}\' doesn\'t exist'.format(lab_name))}
+                return web.Response(text=json.dumps(response_obj), status=404)
+
+            self.labs.pop(lab_name)
+            response_obj = {"status": "success", "message": 'lab was deleted'}
+            return web.Response(text=json.dumps(response_obj), status=200)
+
+        except Exception as ex:
+            response_obj = {"status": "failed", "message": str(ex)}
+            return web.Response(text=json.dumps(response_obj), status=400)
+
+    #  функция для получения имени текущей лабораторной работы
+    def get_lab_name(self, request):
+        return str(request.url).removeprefix(self.url + '/').split('?')[0]
+
+    #  запуск сервера
+    def run(self):
+        web.run_app(self.app, host=self.host, port=self.port)
+
+
+server = Server()
+server.run()
---
+++ lab5.py
@@ -0,0 +1,188 @@
+import asyncio
+import json
+import aiohttp
+import re
+import csv
+
+
+class Client:
+    def __init__(self):
+        self.host = 'localhost'
+        self.port = 8080
+        self.url = 'http://' + self.host + ':' + str(self.port) + '/labs'
+        self.session = None
+        self.reg_for_post = r'\bpost\b:\s*http://localhost:8080/labs\s*{.*}\s*'
+        self.reg_for_patch = r'\bpatch\b:\s*http://localhost:8080/labs/\b\w+\b\s*{.*}\s*'
+        self.reg_for_get_all = r'\bget\b:\s*http://localhost:8080/labs\s*'
+        self.reg_for_get_lab = r'\bget\b:\s*http://localhost:8080/labs/\b\w+\b'
+        self.reg_for_delete_lab = r'\bdelete\b:\s*http://localhost:8080/labs/\b\w+\b'
+        self.sub_reg_for_params = r'{.*}'
+        self.sub_reg_for_url = r'http://localhost:8080/labs/\b\w+\b'
+
+    async def run_session(self):
+        self.session = aiohttp.ClientSession()
+        try:
+            while True:
+                await self.input_to_req()
+        # except EOFError:
+        #     await self.session.close()
+        #     print("Сессия завершена успешно")
+        #     exit(0)
+        except Exception as e:
+            print(e)
+            await self.session.close()
+            print("Сессия завершена")
+            exit(-1)
+
+    async def input_to_req(self):
+        input_text = "Введите запрос: \n"
+        req_string = input(input_text).removeprefix(input_text)
+
+        is_post = re.fullmatch(self.reg_for_post, req_string)
+        is_patch = re.fullmatch(self.reg_for_patch, req_string)
+        is_get = re.match(self.reg_for_get_lab, req_string)
+        is_get_all = re.match(self.reg_for_get_all, req_string)
+        is_delete = re.match(self.reg_for_delete_lab, req_string)
+
+        if is_post:
+            print("Создание лабораторной работы")
+            params_string = re.findall(self.sub_reg_for_params, req_string)[0]
+            params_dict = json.loads(params_string)
+            async with self.session.post(self.url, params=params_dict) as resp:
+                await self.process_create_lab_resp(resp.status, resp)
+
+        elif is_patch:
+            print("Изменение лабораторной работы")
+            params_url = re.findall(self.sub_reg_for_url, req_string)[0]
+            params_string = re.findall(self.sub_reg_for_params, req_string)[0]
+            params_dict = json.loads(params_string)
+            async with self.session.patch(params_url, params=params_dict) as resp:
+                await self.check_resp_is_success(resp.status, resp)
+
+        elif is_get:
+            print("Получение данных о лабораторной работе")
+            params_url = re.findall(self.sub_reg_for_url, req_string)[0]
+            async with self.session.get(params_url) as resp:
+                await self.process_get_lab(resp.status, resp)
+
+        elif is_get_all:
+            print("Получение данных о всех лабораторных работах")
+            async with self.session.get(self.url) as resp:
+                await self.process_get_all_labs(resp.status, resp)
+
+        elif is_delete:
+            print("Удаление лабораторной работы")
+            params_url = re.findall(self.sub_reg_for_url, req_string)[0]
+            async with self.session.delete(params_url) as resp:
+                await self.check_resp_is_success(resp.status, resp)
+        else:
+            print("Запрос введён некорректно")
+
+    @staticmethod
+    async def check_resp_is_success(status, response) -> bool:
+        print(status)
+        if status not in range(200, 299):
+            print(await response.text())
+            return False
+        return True
+
+    async def process_create_lab_resp(self, status, response):
+        if await self.check_resp_is_success(status, response) is False:
+            return
+        d = json.loads(await response.text())
+        d.pop('status', None)
+        print(d)
+        if "Location" in d:
+            print(d["Location"])
+            return
+        else:
+            print("Отсутствует заголовок \'Location\'")
+
+    async def process_get_lab(self, status, response):
+        if await self.check_resp_is_success(status, response) is False:
+            return
+
+        raw_lab_dict = json.loads(await response.text())
+        raw_lab_dict.pop('status', None)
+
+        if len(raw_lab_dict) != 1:
+            print(raw_lab_dict, "Incorrect lab format")
+            return
+
+        lab_name = list(raw_lab_dict.keys())[0]
+        lab_params_dict = json.loads(raw_lab_dict[lab_name])
+        # print(lab_name, lab_params_dict, type(lab_params_dict))
+
+        lab_date = lab_params_dict['deadline']
+        lab_descr = lab_params_dict['description']
+        lab_students_list = lab_params_dict['students'].replace(" ", "").strip().split(",")
+
+        print(lab_name, lab_date, lab_descr, lab_students_list)
+        with open("{}.csv".format(lab_name), 'w', newline='') as csv_file:
+            fields = ['students', 'lab_params']
+            writer = csv.DictWriter(csv_file, fieldnames=fields, delimiter=';')
+            writer.writerow({'lab_params': lab_name})
+            writer.writerow({'lab_params': lab_date})
+            writer.writerow({'lab_params': lab_descr})
+            for student in lab_students_list:
+                if student != '':
+                    writer.writerow({'students': student, 'lab_params': '+'})
+
+    async def process_get_all_labs(self, status, response):
+        if await self.check_resp_is_success(status, response) is False:
+            return
+
+        raw_lab_dict = json.loads(await response.text())
+        raw_lab_dict.pop('status', None)
+
+        fields = ['students']
+        lab_name_dict = dict()
+        lab_date_dict = dict()
+        lab_descr_dict = dict()
+        lab_all_students_list = list()
+        lab_students_dict = dict()
+
+        for lab_name in raw_lab_dict.keys():
+            fields.append(lab_name)
+            lab_params_dict = json.loads(raw_lab_dict[lab_name])
+            lab_name_dict[lab_name] = lab_name
+            lab_date_dict[lab_name] = lab_params_dict['deadline']
+            lab_descr_dict[lab_name] = lab_params_dict['description']
+            lab_students_dict[lab_name] = lab_params_dict['students'].replace(" ", "").strip().split(",")
+            lab_all_students_list += lab_students_dict[lab_name]
+
+        lab_all_students_set = set(lab_all_students_list)
+
+        with open("labs.csv", 'w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=fields, delimiter=';')
+
+            writer.writerow(lab_name_dict)
+            writer.writerow(lab_date_dict)
+            writer.writerow(lab_descr_dict)
+
+            for student in [*lab_all_students_set]:
+                if student == '':
+                    continue
+                student_results = dict()
+                student_results['students'] = student
+                for lab_name in lab_name_dict.keys():
+                    if student in lab_students_dict[lab_name]:
+                        student_results[lab_name] = "+"
+                    else:
+                        student_results[lab_name] = "-"
+                writer.writerow(student_results)
+
+
+print("Введите запрос согласно шаблону: \n"
+      "{тип запроса}: {URL-адрес сервера:порт} {параметры запроса в формате json} \n"
+      "Примеры:\n"
+      "post: http://localhost:8080/labs {\"name\": \"lab1\", \"date\": \"12.12.2021\"}\n"
+      "post: http://localhost:8080/labs {\"name\": \"lab2\", \"date\": \"23.12.2021\"}\n"
+      "patch: http://localhost:8080/labs/lab1 {\"description\" : \"lala\", \"add_students\" : \"Kira, Dima\"}\n"
+      "patch: http://localhost:8080/labs/lab1 {\"description\" : \"lala\", \"delete_students\" : \"Dima\"}\n"
+      "get: http://localhost:8080/labs\n"
+      "get: http://localhost:8080/labs/lab1\n"
+      "delete: http://localhost:8080/labs/lab1\n"
+      "Для завершения сессии нажмите CTRL+E")
+
+asyncio.run(Client().run_session())
